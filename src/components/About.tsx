d9f117fe54e17a70d8d1948573ab72360a6a67e1
Mike Cavaliere <mcavaliere@users.noreply.github.com>
2024-05-24 09:57:45 -0400

    fix: use next/image, add sharp

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,15 +1,13 @@
 import { Statistics } from "./Statistics";
 import pilot from "../assets/pilot.png";
+import Image from "next/image";
 
 export const About = () => {
   return (
-    <section
-      id="about"
-      className="container py-24 sm:py-32"
-    >
+    <section id="about" className="container py-24 sm:py-32">
       <div className="bg-muted/50 border rounded-lg py-12">
         <div className="px-6 flex flex-col-reverse md:flex-row gap-8 md:gap-12">
-          <img
+          <Image
             src={pilot}
             alt=""
             className="w-[300px] object-contain rounded-lg"
--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -9,11 +9,13 @@ import {
 import image from "../assets/growth.png";
 import image3 from "../assets/reflecting.png";
 import image4 from "../assets/looking-ahead.png";
+import Image from "next/image";
+import { StaticImageData } from "next/image";
 
 interface FeatureProps {
   title: string;
   description: string;
-  image: string;
+  image: StaticImageData;
 }
 
 const features: FeatureProps[] = [
@@ -51,10 +53,7 @@ const featureList: string[] = [
 
 export const Features = () => {
   return (
-    <section
-      id="features"
-      className="container py-24 sm:py-32 space-y-8"
-    >
+    <section id="features" className="container py-24 sm:py-32 space-y-8">
       <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
         Many{" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
@@ -65,10 +64,7 @@ export const Features = () => {
       <div className="flex flex-wrap md:justify-center gap-4">
         {featureList.map((feature: string) => (
           <div key={feature}>
-            <Badge
-              variant="secondary"
-              className="text-sm"
-            >
+            <Badge variant="secondary" className="text-sm">
               {feature}
             </Badge>
           </div>
@@ -85,7 +81,7 @@ export const Features = () => {
             <CardContent>{description}</CardContent>
 
             <CardFooter>
-              <img
+              <Image
                 src={image}
                 alt="About feature"
                 className="w-[200px] lg:w-[300px] mx-auto"
